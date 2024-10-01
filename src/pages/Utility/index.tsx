00e895ff6f4b1ffcc14ce4cceae94e9c0c3fffc9
denerbatista <denerbatista@icloud.com>
2024-10-01 10:25:34 -0300

    feat: create page Utility and componente Card

---
+++ src/pages/Utility/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+import Card from "../../components/Card";
+
+const Utility: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Main>
+        <Card />
+      </Main>
+    </>
+  );
+};
+
+export default Utility;
